a7f252dea735d669c5f9f6d0f52b280fefb8a0fb
Mikask <darrenprasetya40@gmail.com>
2025-09-30 15:47:25 +0700

    feat: add google service account envs

--- src/lib/createSheetsClient.ts
+++ src/lib/createSheetsClient.ts
@@ -5,13 +5,16 @@ import { google, sheets_v4 } from "googleapis";
  * @returns {Promise<sheets_v4.Sheets>} The Google Sheets API client.
  */
 export default async function createSheetsClient(): Promise<sheets_v4.Sheets> {
-    const credentialsJson = process.env.GOOGLE_SERVICE_ACCOUNT_CREDENTIALS;
-    if (!credentialsJson) {
-        throw new Error("🛑 GOOGLE_SERVICE_ACCOUNT_CREDENTIALS environment variable is not set");
-    }
-    const credentials = JSON.parse(credentialsJson);
     const auth = new google.auth.GoogleAuth({
-        credentials,
+        credentials: {
+            type: process.env.GOOGLE_SERVICE_ACCOUNT_TYPE,
+            project_id: process.env.GOOGLE_SERVICE_ACCOUNT_PROJECT_ID,
+            private_key_id: process.env.GOOGLE_SERVICE_ACCOUNT_PRIVATE_KEY_ID,
+            private_key: process.env.GOOGLE_SERVICE_ACCOUNT_PRIVATE_KEY,
+            client_email: process.env.GOOGLE_SERVICE_ACCOUNT_CLIENT_EMAIL,
+            client_id: process.env.GOOGLE_SERVICE_ACCOUNT_CLIENT_ID,
+            universe_domain: process.env.GOOGLE_SERVICE_ACCOUNT_UNIVERSE_DOMAIN
+        },
         scopes: ["https://www.googleapis.com/auth/spreadsheets"],
     });
     const sheets = google.sheets({ version: "v4", auth });
